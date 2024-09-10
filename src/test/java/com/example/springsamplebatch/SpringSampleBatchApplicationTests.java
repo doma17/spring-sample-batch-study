e39265659ce3a8de1328e9cbf58fc00e30c9e10d
kbm <rovin1273@gmail.com>
2024-09-10 15:35:26 +0900

    Frist Commit

---
+++ src/test/java/com/example/springsamplebatch/SpringSampleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsamplebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
