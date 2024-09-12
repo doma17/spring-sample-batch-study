7cd2a0eeef9e38d32c9fb9a068c8179bc93446bf
kbm <rovin1273@gmail.com>
2024-09-12 18:40:24 +0900

    add : Add FifthBatch Example

--- src/main/java/com/example/springsamplebatch/controller/MainController.java
+++ src/main/java/com/example/springsamplebatch/controller/MainController.java
@@ -50,4 +50,15 @@ public class MainController {
 
         return "ok";
     }
+
+    @GetMapping("/fifth")
+    public String fifthApi(@RequestParam("value") String value) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("data", value)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("fifthJob"), jobParameters);
+
+        return "ok";
+    }
 }
